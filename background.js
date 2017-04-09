114d15ebedb96dc853aceb851ca53c2053c21940
Peter <petercheung016@gmail.com>
2017-04-09 01:16:41 -0400

    Changes to Memeify

--- background.js
+++ background.js
@@ -2,3 +2,19 @@ var audio = new Audio('allstar.mp3');
 audio.play();
 var start = document.getElementById('start');
 var stop = document.getElementById('stop');
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  var checkPageButton = document.getElementById('memePage');
+  checkPageButton.addEventListener('click', function() {  	
+
+  // Executes the js file that replaces all the images on the page
+  chrome.tabs.executeScript(null, {
+    file: "memeify.js"
+  });
+
+  }, false);
+}, false);
+
+
--- test.js
+++ test.js
@@ -1,4 +1,3 @@
-/* Implements the button */
   var images = document.getElementsByTagName('img'), length = images.length;
   
   for (var i = 0; i < length; i++) {
