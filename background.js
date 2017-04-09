59c570d8795a23c189247dab65ce7a98ca25c9be
Peter <petercheung016@gmail.com>
2017-04-09 04:58:31 -0400

    Cool memes

--- background.js
+++ background.js
@@ -1,4 +1,3 @@
-
 var rng = Math.floor(Math.random() * 3);
 var audio = new Audio("/Music/"+ rng +".mp3");
 audio.play();
@@ -7,6 +6,8 @@ audio.play();
 document.addEventListener('DOMContentLoaded', function() {
   var checkPageButton = document.getElementById('memePage');
   checkPageButton.addEventListener('click', function() {  	
+  	var audio = new Audio("/Music/airhorn.mp3");
+	audio.play();
 
   // Executes the js file that replaces all the images on the page
   chrome.tabs.executeScript(null, {
--- memeify.js
+++ memeify.js
@@ -1,5 +1,6 @@
 /* Implements the button */
 var images = document.getElementsByTagName('img'), length = images.length;
+var words = document.getElementsByTagName('*'), tlength = words.length;
   
 var memes = [
 	'http://www.wpromote.com/blog/wp-content/uploads/2015/06/morpheus-meme.jpg',
@@ -11,37 +12,41 @@ var memes = [
 	'http://i0.kym-cdn.com/entries/icons/medium/000/003/619/Untitled-1.jpg',
 	'https://img.memesuper.com/c290bc11e019c403771d87c87909b7ee_popular-memes-image-memes-at-memes-most-popular_600-392.jpeg',
 	'https://s-media-cache-ak0.pinimg.com/736x/b0/09/07/b00907cd233b66d07a051ed45f2e46aa.jpg',
-	'http://i3.kym-cdn.com/entries/icons/original/000/006/151/tumblr_lltzgnHi5F1qzib3wo1_400.jpg'
-
+	'http://i3.kym-cdn.com/entries/icons/original/000/006/151/tumblr_lltzgnHi5F1qzib3wo1_400.jpg',
+	'https://media2.giphy.com/media/ehc19YLR4Ptbq/giphy.gif',
+	'https://img.memesuper.com/1a9018436e732bf7df11b5c0a894eed2_2048-meme-gif-owned-meme-gif_400-400.gif',
+	'https://media.giphy.com/media/rjibxsFWbKQ8/giphy.gif',
+	'https://media.tenor.co/images/20828bc040c2d7dd237de8e0a9803a0e/tenor.gif',
+	'https://media.tenor.co/images/4ec2a04fd4553fd23d4742108889037c/tenor.gif',
+	'https://img.memesuper.com/d51e6823867672de9ea439503738aa8a_4chans-frog-meme-went-pepe-memes-sad_550-535.jpeg',
+	'https://media.tenor.co/images/1c59360c9836f5fb771a9293fee63327/tenor.gif',
+	'https://media1.giphy.com/media/SduJilGpbXqAE/giphy.gif',
+	'http://picsmine.com/wp-content/uploads/2017/02/LOL-Meme-LOLOLLOLOLOL.gif',
+	'https://media.giphy.com/media/26FPr0GKKPaPiUbbG/giphy.gif',
+	'http://pa1.narvii.com/5858/c5bea9a6d774cc07af1873b2e3ffc472dd73c538_hq.gif',
+	'https://secure.static.tumblr.com/e1c5f97b9791ce572b6065e19b5adcee/1dpn6ec/wJHobxk6f/tumblr_static_tumblr_static__640.gif',
+	'http://ci.memecdn.com/7790552.gif',
+	'https://img.memesuper.com/15106c886713d49d31abf1ff5e260f58_1000-images-about-gif-on-funny-memes-gif_455-364.gif',
+	'https://img.memesuper.com/e69021f47f73e7d5effe56961f0ada1d_animated-funny-memes-gif-funny-memes-gif_496-312.gif',
+	'https://media.giphy.com/media/CdXGXGX8WUgpi/giphy.gif',
+	'http://cdn2-www.craveonline.com/assets/mandatory/legacy/2016/03/man_file_1065676_962755.gif',
+	'https://m.popkey.co/e970db/LWqyp.gif',
+	'https://s-media-cache-ak0.pinimg.com/originals/51/67/06/51670674e34e8d40da4d4eedc07d4bea.gif',
+	'https://s-media-cache-ak0.pinimg.com/originals/09/65/1b/09651bb32e3932f2cf4b55dab8c16db6.gif',
+	'http://acidcow.com/pics/20111026/hilarious_drunk_and_wasted_people_32.gif'
 ];
 
 
 for (var i = 0; i < length; i++) {
 	var memeIndex = Math.floor(getRandomImage(memes)); //Gets index of memes array
     images[i].src = memes[memeIndex];
+    document.body.style.backgroundImage = "url('https://media1.giphy.com/media/gGs07Iz8IfiE0/giphy.gif')";
  }
 
+ for (var i = 0; i < length; i++) {
+    words[i].src = "dank";
+ }
 
 function getRandomImage(imageArray) {
   	return (Math.random() * imageArray.length);
 }
-
-
-// function scaleImage(imageToReplace) {
-// 	img = new Image();
-// 	img.src = imageToReplace.src;
-// 	// img 
-// 	  	var canvas = document.createElement('canvas');
-// 	  	var width = imageToReplace.width;
-// 	  	var height = imageToReplace.height;
-// 		var ctx = canvas.getContext('2d');
-	    
-// 	    canvas.width = width;
-// 	    canvas.height = height;
-// 	    ctx.drawImage(img, 0, 0, width, height);
-
-// 	    return canvas.toDataURL();
-// 	    // img.width = width;
-// 	    // img.height = height;
-// 	    // imageToReplace.src = img.src;
-// }
