d71b1e2c07853159f5f18d5ac46963d05d6c50c8
pcheung16 <petercheung016@gmail.com>
2017-04-09 17:48:21 -0400

    Delete test.js

--- test.js
+++
@@ -1,5 +0,0 @@
-  var images = document.getElementsByTagName('img'), length = images.length;
-  
-  for (var i = 0; i < length; i++) {
-    images[i].src = 'https://img.memesuper.com/c31bb0058159bec30f9952dd049fb6f5_this-could-work-out-well-meme-pics_400-400.jpeg';
-  }
