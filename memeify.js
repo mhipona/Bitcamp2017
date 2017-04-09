8022f5b5f67283ca73f07593c6551445f51456ca
Marc Hipona <marchipona@yahoo.com>
2017-04-08 22:58:35 -0400

    Added button implementation

--- memeify.js
+++ memeify.js
@@ -1,1 +1,10 @@
 /* Where the magic happens */
+
+/* Implements the button */
+document.addEventListener('DOMContentLoaded', function() {
+  var checkPageButton = document.getElementById('checkPage');
+  checkPageButton.addEventListener('click', function() {
+
+    
+  }, false);
+}, false);
